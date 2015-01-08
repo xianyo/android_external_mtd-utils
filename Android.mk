234f9d7aecb23057d565745d1740bcf8551ccde0
zhang sanshan <b51434@freescale.com>
2015-01-08 09:47:12 +0800

    MA-6096 enable selinux context to be added to ubifs images with mkfs.ubifs
    1 change the android.mk to support selinux context
    2 add option "-s" for mkfs.ubifs
    
    Signed-off-by: zhang sanshan <b51434@freescale.com>

--- Android.mk
+++ Android.mk
@@ -8,7 +8,7 @@ include $(CLEAR_VARS)
 
 LOCAL_CFLAGS += -O2 -Wall
 LOCAL_LDLIBS += -lz -lm 
- 
+LOCAL_STATIC_LIBRARIES := libselinux 
 LOCAL_SRC_FILES := \
        ubi-utils/src/libubi.c \
        mkfs.ubifs/mkfs.ubifs.c \
@@ -32,7 +32,8 @@ LOCAL_C_INCLUDES += \
         $(LOCAL_PATH)/ubi-utils/include \
         $(LOCAL_PATH)/ubi-utils/src \
         $(LOCAL_PATH)/lzo-2.03/include \
-        $(LOCAL_PATH)/uuid/  
+        $(LOCAL_PATH)/uuid/  \
+	external/libselinux/include 
 
 LOCAL_MODULE := mkfs_ubifs
 
