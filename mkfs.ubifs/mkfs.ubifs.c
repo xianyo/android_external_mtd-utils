ceaab1e15d832d2d21144a409a0bebbdbaa84e34
zhang sanshan <b51434@freescale.com>
2015-01-08 09:47:12 +0800

    MA-6096 enable selinux context to be added to ubifs images with mkfs.ubifs
    1 change the android.mk to support selinux context
    2 add option "-s" for mkfs.ubifs
    
    Signed-off-by: zhang sanshan <b51434@freescale.com>

--- mkfs.ubifs/mkfs.ubifs.c
+++ mkfs.ubifs/mkfs.ubifs.c
@@ -143,7 +143,7 @@ static struct inum_mapping **hash_table;
 /* Inode creation sequence number */
 static unsigned long long creat_sqnum;
 
-static const char *optstring = "d:r:m:o:D:h?vVe:c:g:f:P:k:x:X:j:R:l:j:U:F";
+static const char *optstring = "d:r:m:o:D:h?vVe:c:g:f:P:k:x:X:j:R:l:j:U:Fs:";
 
 static const struct option longopts[] = {
 	{"root",          1, NULL, 'r'},
